b11e1856f3f429f077b89d323581f533155dae7c
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-20 18:22:24 +0530

    Add examples for permutation, max/min elements, and popcount
    Added examples for __builtin_popcount and __builtin_popcountll.

--- stl.cpp
+++ stl.cpp
@@ -28,3 +28,24 @@ int main(){
     cout<< maxi;
     return 0;
 }
+
+//__builtin_popcount() return the how many 1's in the binary number
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num =9;
+    int n = __builtin_popcount(num);
+    cout << n;
+    return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long  num =5678;
+    int n = __builtin_popcountll(num);
+    cout << n;
+    return 0;
+}
