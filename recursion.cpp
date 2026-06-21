b1eeefee0c4a29dd8a45ca49f1d497f3eac7edce
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-21 22:21:24 +0530

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -12,3 +12,24 @@ int main(){
     print();
     return 0;
 }
+
+//recursion with base case condition 
+
+#include<bits/stdc++.h>
+using namespace std;
+int cmt = 0;
+void print(){
+    if (cmt == 4){
+        return;
+    }else{
+        cout<<cmt<<endl;
+        cmt++;
+        print();
+    }
+}
+
+int main(){
+    int cmt = 0;
+    print();
+    return 0;
+}
