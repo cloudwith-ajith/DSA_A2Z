7510afdee04a18bb1b8121ccfa66e3ccbf452292
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-23 21:08:03 +0530

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -213,4 +213,27 @@ int main() {
 }
 
 
+//factorial using the functional method 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n){
+    if(n == 1){
+        return 1;
+    }else{
+        return n * factorial(n-1);
+    }
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<factorial(n);
+    return 0;
+} 
+
+
+
 
