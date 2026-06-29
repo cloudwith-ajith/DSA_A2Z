f28df650f1f61c4618a4c56bb2ad82ca5f3fbed5
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-29 21:08:08 +0530

    Update basics_revision.cpp

--- basics_revision.cpp
+++ basics_revision.cpp
@@ -15,3 +15,21 @@ int main(){
     return 0;
 }
 // Time complexity of the problem is O(n)
+
+// using hash array 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,4,1,1,1,1,1,3};
+    int hash[5] = {0};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i =0;i < n;i++){
+        hash[arr[i]] = hash[arr[i]] + 1;
+    }
+    int target = 1;
+    cout<<hash[target];
+    return 0;
+}
+
+// TC os this is O(1) + O(n) as per the rule avoid the constant value --> O(n)
