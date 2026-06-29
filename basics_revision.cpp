5b2b6e15377b3595bef12d4b47fd299ed123d6dc
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-29 21:19:38 +0530

    Update basics_revision.cpp

--- basics_revision.cpp
+++ basics_revision.cpp
@@ -48,3 +48,18 @@ int main(){
     cout<<mpp[target];
     return 0;
 }
+
+// frequence in string 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "ajithkumar";
+    int hash[26] ={0};
+    for(int i = 0; i < s.length();i++){
+       hash[s[i]-'a']++; 
+    }
+    char c = 'a';
+    cout<<hash[c -'a'];
+    return 0;
+}
