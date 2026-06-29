3686f68ce50bef15f1455b06556fce11fe853c4c
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-29 21:13:49 +0530

    Update basics_revision.cpp

--- basics_revision.cpp
+++ basics_revision.cpp
@@ -33,3 +33,18 @@ int main(){
 }
 
 // TC os this is O(1) + O(n) as per the rule avoid the constant value --> O(n)
+
+//hashing using the unordered map
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,4,1,1,1,1,5,6,7};
+    unordered_map <int,int> mpp;
+    for(int i = 0;i < sizeof(arr)/sizeof(arr[0]);i++){
+        mpp[arr[i]]++;
+    }
+    int target = 1;
+    cout<<hash[target];
+    return 0;
+}
