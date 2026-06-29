e57945d8ae720bfb9e38e7a97c29a459ce306cd5
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-29 21:00:32 +0530

    Update basics_revision.cpp

--- basics_revision.cpp
+++ basics_revision.cpp
@@ -1,1 +1,17 @@
-
+//counting the number in brute force method
+#include<bits/stdc++.h>
+using namespace std;
+// normal method 
+int main(){
+    int arr[] = {1,2,3,4,1,1,1,1,3};
+    int count = 0;
+    int target = 1;
+    for(int i =0;i < sizeof(arr)/sizeof(arr[0]);i++){
+        if(arr[i] == target){
+            count++;
+        }
+    }
+    cout<<target<<"---->"<<count;
+    return 0;
+}
+// Time complexity of the problem is O(n)
