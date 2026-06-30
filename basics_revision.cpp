8bfa649598041b910d9d719093b425a9214923cb
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-30 21:46:05 +0530

    Update basics_revision.cpp

--- basics_revision.cpp
+++ basics_revision.cpp
@@ -130,3 +130,27 @@ int main(){
     cout<< maxx<< " ";
     return 0;
 }
+
+------------------------------------------------recursion--------------
+
+
+ //fibonacci series   
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int fibo(int n){
+    if (n <= 1){
+        return n;
+    }else{
+        return fibo(n-2) + fibo(n-1);
+    }
+}
+
+
+int main(){
+    int n = 5;
+    int a = fibo(n);
+    cout<<a;
+    return 0;
+}
