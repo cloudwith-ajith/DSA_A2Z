10788172bec5d7be4291027dcaef240a9a454aa5
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-06-30 21:50:41 +0530

    Update basics_revision.cpp

--- basics_revision.cpp
+++ basics_revision.cpp
@@ -257,3 +257,23 @@ int main(){
     cout<<fac(n);
     return 0;
 }
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int i){
+    if (i < 1 ){
+        return ;
+    }else{
+        cout<<i<<" ";
+        print(i-1);
+    }
+}
+
+int main(){
+    int n = 5;
+    print(n);
+    return 0;
+}
+
