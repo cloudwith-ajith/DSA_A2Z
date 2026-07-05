c2785777af3d86ac338e760de418b3f7757c1d55
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-05 21:38:45 +0530

    Update basics_revision.cpp

--- basics_revision.cpp
+++ basics_revision.cpp
@@ -602,3 +602,20 @@ int main(){
 //--------------------map,unordered_map,multimap---------------
 // can iterate,unlike stack, queue, pq
 
+//*******************************************************************************************************
+//math
+
+//count the digits
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 12345;
+    int cnt = 0;
+    while(n != 0){
+        n =n / 10;
+        cnt += 1;
+    }
+    cout<<cnt;
+    return 0;
+}
