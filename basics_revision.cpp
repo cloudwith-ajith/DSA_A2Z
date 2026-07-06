b986a442768d08412424e74415ebac514dd73156
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-06 21:53:26 +0530

    Update basics_revision.cpp

--- basics_revision.cpp
+++ basics_revision.cpp
@@ -786,3 +786,24 @@ int main(){
 // 1 2 3 
 // 1 2 
 // 1 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    for(int i = 1; i<5 ; i++){
+        for(int j = 5; j>i;j--){
+            cout<<"  ";
+        }
+        for(int k = 1; k <= ((i * 2)-1);k++){
+            cout<<"* ";
+        }cout<<endl;
+    }
+    
+    return 0;
+}
+  //       * 
+  //     * * * 
+  //   * * * * * 
+  // * * * * * * * 
