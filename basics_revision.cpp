f247e753b3acd6b2ce4e92cf170726036ba2d8c5
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-07 21:56:42 +0530

    Update basics_revision.cpp

--- basics_revision.cpp
+++ basics_revision.cpp
@@ -892,3 +892,40 @@ int main(){
 // 4 5 6 
 // 7 8 9 10 
 // 11 12 13 14 15
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n = 5;
+
+    for(int i = 1; i <= n; i++){
+        if(i == 1 || i ==n ){
+            for(int j = 1; j <= n; j++){
+                cout<<"* ";
+            }
+        }else{
+            for(int k = 1; k <= n;k++){
+                if(k == 1|| k == n){
+                    cout<<"* ";
+                }else{
+                    cout<<"  ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
+
+
+// * * * * * 
+// *       * 
+// *       * 
+// *       * 
+// * * * * * 
+
+
