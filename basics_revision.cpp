0b772ac1eccff17cb96273e82518ccfc0d2b9481
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-07 21:57:54 +0530

    Update basics_revision.cpp

--- basics_revision.cpp
+++ basics_revision.cpp
@@ -965,3 +965,33 @@ int main() {
     return 0;
 }
 
+-----------------------------------------------------
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n =4;
+    for(int i = 0; i < (2 *n)-1;i++ ){
+        for(int j = 0; j < (2*n)-1;j++){
+            int top = i;
+            int left = j;
+            int right = (2*n-2)-j;
+            int down = (2*n-2) - i;
+
+            cout<<(n-min(min(top,down),min(right,left)))<<" ";
+        }cout<<endl;
+    }
+
+
+    return 0;
+}
+
+
+// 4 4 4 4 4 4 4 
+// 4 3 3 3 3 3 4 
+// 4 3 2 2 2 3 4 
+// 4 3 2 1 2 3 4 
+// 4 3 2 2 2 3 4 
+// 4 3 3 3 3 3 4 
+// 4 4 4 4 4 4 4 
+
