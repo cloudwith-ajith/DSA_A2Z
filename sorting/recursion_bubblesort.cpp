60b8a93d74a3e6d71e4228e97a2a1fd564c8934b
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-14 22:16:23 +0530

    Implement recursive bubble sort algorithm

--- sorting/recursion_bubblesort.cpp
+++ sorting/recursion_bubblesort.cpp
@@ -19,3 +19,35 @@ int main(){
     }
     return 0;
 }
+
+------------------------------------------
+    
+//recurssion--bubble_sort
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[],int n ){
+    if(n==0) {return;}
+    
+    for(int i = 0 ; i <= n-1; i++){
+        if (arr[i] > arr[i+1]){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    
+    bubble_sort(arr, n-1);
+}
+
+
+int main(){
+    
+    int arr [6] ={13,46,24,52,20,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubble_sort(arr,n-1);
+    
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
