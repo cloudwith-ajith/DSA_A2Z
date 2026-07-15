3cc06f70b2fc10038bed94cb57a934a30283bf0b
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-15 22:01:36 +0530

    Revise merge sort implementation in Mergesort.cpp

--- sorting/Mergesort.cpp
+++ sorting/Mergesort.cpp
@@ -66,3 +66,57 @@ for (int i = low; i <= high; i++) {
     arr[i] = temp[t]; 
     t++; // Move to the next item in temp
 }
+---------------------revesion
+
+    ---------merge sort revsion 
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[], int low, int mid, int high){
+    vector <int > temp;
+    int i = low;
+    int j = mid+1;
+    
+    while(i <= mid && j <= high){
+        if(arr[i] > arr[j]){
+            temp.push_back(arr[j]);
+            j++;
+        }else{
+            temp.push_back(arr[i]);
+            i++;
+        }
+    }
+    while(i <= mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while(j <= high){
+        temp.push_back(arr[j]);
+        j++;
+    }
+    for(int i  = low; i <= high; i++){
+        arr[i] = temp[i -low];
+    }
+}
+
+void merge_sort(int arr[],int low, int high){
+    if (low >= high){
+        return;
+    }else{
+        int mid = (low + high) / 2;
+        merge_sort(arr,low,mid);
+        merge_sort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+}
+
+
+int main(){
+    int arr[6] ={4,1,29,3,14,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    merge_sort(arr,0,n-1);
+    for(int i : arr){
+        cout<<i <<" ";
+    }
+    return 0;
+}
