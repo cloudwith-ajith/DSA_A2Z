b261343281e1a039f84a0459cfcb8bb34ee804d8
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-16 22:32:07 +0530

    Update reversion.cpp

--- sorting/reversion.cpp
+++ sorting/reversion.cpp
@@ -166,5 +166,28 @@ int main(){
     }
     return 0;
 }
+//------------selection sort 
 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[8] = {1,46,24,52,20,9,2,13};
+     int n = sizeof(arr)/sizeof(arr[0]);
+     
+     for(int i = 0;i < n-1; i++){
+         int min = i;
+         for(int j = i; j< n; j++){
+             if(arr[j] < arr[min]){
+                 min = j;
+             }
+         }
+         swap(arr[i],arr[min]);
+     }
+     
+     for(int i : arr){
+         cout<<i <<" ";
+     }
+    return 0;
+}
 
