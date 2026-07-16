5838664e393c4fe302d9bc62d1cc44d81465ddd0
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-16 22:29:53 +0530

    Update reversion.cpp

--- sorting/reversion.cpp
+++ sorting/reversion.cpp
@@ -40,3 +40,42 @@ int main(){
     for(int i : arr) cout<<i<<" ";
     return 0;
 }
+//---------------------------------------quick sort 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int partition(int arr[],int low,int high){
+    int pivot = arr[high];
+    int i = low - 1;
+    
+    for(int j = low; j < high; j++){
+        if(arr[j] <= pivot){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    
+    swap(arr[high],arr[i+1]);
+    return i + 1;
+}
+
+
+void Quick_sort(int arr[],int low,int high){
+    if (low >= high){
+        return ;
+    }else{
+        int sorted_index = partition(arr,low,high);
+        Quick_sort(arr,low,sorted_index -1);
+        Quick_sort(arr,sorted_index + 1,high);
+    }
+}
+int main(){
+    int arr[6] = {34,34,23,32,5,62};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    Quick_sort(arr,0,n-1);
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
