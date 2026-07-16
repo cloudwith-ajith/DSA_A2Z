3b49086c53a8744f8ff7df929c94491b524efff0
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-16 22:31:41 +0530

    Update reversion.cpp

--- sorting/reversion.cpp
+++ sorting/reversion.cpp
@@ -144,3 +144,27 @@ int main(){
     return 0;
 }
 
+//-------------bubble sort 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[6] = {34,7,23,32,5,62};
+    int n  = sizeof(arr) / sizeof(arr[0]);
+    
+    for(int i = n-1; i >= 0; i--){
+        for(int j = 0 ; j < i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
+
+
