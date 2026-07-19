5ab13a5961a7cc28d69c1d1a6cc85f28c448026c
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-19 19:34:10 +0530

    Update Left Rotate the Array by One.cpp

--- Array/Left Rotate the Array by One.cpp
+++ Array/Left Rotate the Array by One.cpp
@@ -1,4 +1,4 @@
-//brute force method Tc - o(n),sp - o(n)
+//---------------------------------brute force method Tc - o(n),sp - o(n)
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -21,5 +21,28 @@ int main(){
     int n = sizeof(arr)/sizeof(arr[0]);
     rotate(arr,n);
   
+    return 0;
+}
+//-------------------------------------optimal approach TC is - O(n), SP O(1)---
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotate(int arr[],int n ){
+    int first_element =arr[0];
+    
+    for(int i =1;i < n;i++){
+        arr[i-1] =arr[i];
+    }
+    arr[n-1]=first_element;
+} 
+int main(){
+    int arr[] ={-1, 0, 3, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    rotate(arr,n);
+    
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+
     return 0;
 }
