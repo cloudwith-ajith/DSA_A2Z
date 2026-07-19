680c722780c8ef281b53df3bc5e25df4b653dc96
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-19 23:34:05 +0530

    Update left_rotate_k_elements.cpp

--- Array/left_rotate_k_elements.cpp
+++ Array/left_rotate_k_elements.cpp
@@ -30,3 +30,34 @@ int main(){
     }
     return 0;
 }
+
+//----------- brute force with math
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotate(int arr[],int k,int n ){
+    int temp_arr[k];
+    for(int i =0;i < k;i++){
+        temp_arr[i] = arr[i];
+    }
+    
+    for(int j = k; j < n;j++){
+        arr[j - k] =arr[j];
+    }
+    for(int m = n-k; m < n; m++){
+        arr[m] = temp_arr[m-(n-k)];
+    }
+}
+
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int k = 2;
+    int n = size(arr);
+    rotate(arr,k,n);
+    
+    for(int a : arr){
+        cout<<a<<" ";
+    }
+    return 0;
+}
