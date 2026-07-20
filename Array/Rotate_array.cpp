8b3651775a70c22a621a07f72e4f84a7ef16f13c
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-20 22:20:25 +0530

    Create Rotate_array.cpp

---
+++ Array/Rotate_array.cpp
@@ -0,0 +1,40 @@
+//--------------------------------------right rotate
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(int arr[],int start, int end){
+    while(start < end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void k_rotate(int arr[],int k,int n){
+    //normalize k = k % n. egde case
+    k = k % n;
+    
+    //reverse the entire array
+    reverse(arr,0,n-1);
+    
+    //reverse the first part
+    reverse(arr,0, k-1);
+    
+    //reverse the last part
+    reverse(arr,k,n-1);
+    
+    
+}
+
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 2;
+    k_rotate(arr,k,n);
+    
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
