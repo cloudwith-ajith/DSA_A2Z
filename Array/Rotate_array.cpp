5c7c4c59b327e6e9b6f9a839a73f1f4b31e9c5d4
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-20 22:20:51 +0530

    Implement left rotation of array
    Added a function to rotate an array to the left by k positions using reversal algorithm.

--- Array/Rotate_array.cpp
+++ Array/Rotate_array.cpp
@@ -38,3 +38,40 @@ int main(){
     }
     return 0;
 }
+
+//-----------------left rotate
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(int arr[],int start, int end){
+    while(start < end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void k_rotate(int arr[],int k,int n){
+    //normalize k = k % n. egde case
+    k = k % n;
+    // rotate the first k element
+    reverse(arr, 0 , k-1);
+    // rotate the balance element
+    reverse(arr,k,n-1);
+    //reverse the whole array
+    reverse (arr,0,n-1);
+}
+
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 2;
+    k_rotate(arr,k,n);
+    
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
