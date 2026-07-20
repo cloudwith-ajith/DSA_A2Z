9b024535720fc9433546600b2015f5ba24ac25a1
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-20 22:22:44 +0530

    Update Zero_to_end.cpp

--- Array/Zero_to_end.cpp
+++ Array/Zero_to_end.cpp
@@ -29,3 +29,38 @@ int main(){
     }
     return 0;
 }
+
+//--------------------------------------------optimal method
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void move_zero(int arr[],int n ){
+    //assign the j pointer to the zero,but we need to find the zero so we assign as the -1;
+    int j = -1;
+    for(int i = 0; i < n;i++){
+        if(arr[i] ==0){
+            j = i;
+            break;
+        }
+    }
+    
+    for(int i = j + 1; i < n; i++){
+        if(arr[i] != 0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+}
+
+int main(){
+    int arr[] ={1,0,2,3,2,0,0,4,5,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    move_zero(arr,n);
+    
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
+
