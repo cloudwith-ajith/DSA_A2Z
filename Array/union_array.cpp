5c979d830051de5f618ea6627a47214f16450831
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-20 22:24:30 +0530

    Implement optimal union of two arrays
    Added an optimal approach for finding the union of two arrays using vectors.

--- Array/union_array.cpp
+++ Array/union_array.cpp
@@ -24,3 +24,61 @@ int main(){
     
     return 0;
 }
+
+//---------------optimal approach
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> unionarr(vector<int> arr1,vector<int>arr2){
+    int a1_size = arr1.size();
+    int a2_size = arr2.size();
+    vector <int> temp;
+    int i = 0;
+    int j = 0;
+    while(i < a1_size && j < a2_size){
+        if(arr1[i] > arr2[j]){
+            if( temp.size() == 0 || temp.back() != arr2[j]){
+                temp.push_back(arr2[j]);
+            }
+            j++;
+        }else if(arr1[i] < arr2[j]){
+            if(temp.size() == 0 || temp.back() != arr1[i]){
+                temp.push_back(arr1[i]);
+            }
+            i++;
+        }else {
+            if(temp.size() == 0 || temp.back() != arr1[i]){
+                temp.push_back(arr1[i]);
+            }
+            i++;
+            j++; // Move both pointers forward
+        }
+    }
+    
+    while(i < a1_size){
+        if(temp.size() == 0 || temp.back() != arr1[i]){
+                temp.push_back(arr1[i]);
+        }i++;
+    }
+    
+    while(j < a2_size){
+        if( temp.size() == 0 || temp.back() != arr2[j]){
+                temp.push_back(arr2[j]);
+        }
+        j++;
+    }
+    return temp;
+}
+int main(){
+    
+    vector <int> arr1 ={1,2,3,4,5,6,7,8,9,10};
+    vector <int> arr2 ={1,2,3,4,7,8};
+    vector<int> result = unionarr(arr1,arr2);
+    
+    for(int i : result){
+        cout<<i<<" ";
+    }
+    return 0;
+    
+}
