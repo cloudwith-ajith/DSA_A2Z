b3c525ae410ebeb8390b193578537cbcb1f56470
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-21 23:12:22 +0530

    Update 1to9.cpp

--- Array/Revesion/1to9.cpp
+++ Array/Revesion/1to9.cpp
@@ -19,3 +19,28 @@ int main(){
 
 //tc is O(n log n) because its using the sorting 
 // sc is O(1)
+
+//------------------optimal way
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int largest(int arr[],int n){
+    int i = arr[0];
+    for(int j = 1; j < n; j++){
+        if(i < arr[j]){
+            i = arr[j];
+        }
+    }
+    return i;
+}
+
+int main(){
+    int arr[] = {2,5,1,3,0};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<largest(arr,n);
+    return 0;
+}
+
+// tc is O(n) linear to the end 
+//sp is O(1)
