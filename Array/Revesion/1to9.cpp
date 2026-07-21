71ce99e24e26b91d953b091164a6651f1eae78e1
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-21 23:13:54 +0530

    Update 1to9.cpp

--- Array/Revesion/1to9.cpp
+++ Array/Revesion/1to9.cpp
@@ -137,3 +137,30 @@ int main(){
     return 0;
 }
 
+//----------------brute force method
+//----using set tc is O(n log n)
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int arr[] = {1,1,2,2,2,3,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    set <int> temp;
+    
+    for(int i = 0; i < n; i++){
+        temp.emplace(arr[i]);
+    }
+    int j = 0;
+    for(int i : temp){
+        arr[j] = i;
+        j++;
+    }
+    
+    for(int  n : arr){
+        cout<<n<<" ";
+    }
+    return 0;
+}
+
+
