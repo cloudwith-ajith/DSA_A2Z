35a82548112a75da2e94356b9f9e114111dd292f
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-22 23:24:57 +0530

    Update Find_the_missing_number.cpp

--- Array/Find_the_missing_number.cpp
+++ Array/Find_the_missing_number.cpp
@@ -39,3 +39,31 @@ int main(){
     cout<<o_sum -sum;
     return 0;
 }
+
+//---------------------------------------XOR method
+#include<bits/stdc++.h>
+using namespace std;
+
+int mis(int arr[],int n){
+    int xor1 = 0;
+    int xor2 = 0;
+    
+    for(int i = 0; i < n; i++){
+        xor1 = xor1^arr[i];
+        xor2 = xor2^(i+1);
+    }
+    
+    xor2 = xor2 ^ (n+1);
+    //missing value
+    return xor1^xor2;
+}
+
+
+
+int main(){
+    int arr[] = {1,2,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int result = mis(arr,n);
+    cout<<result;
+    return 0;
+}
