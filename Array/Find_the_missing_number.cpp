415de0cf7eda9ec5fad73b83d12d0885b2ead961
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-22 23:24:34 +0530

    Update Find_the_missing_number.cpp

--- Array/Find_the_missing_number.cpp
+++ Array/Find_the_missing_number.cpp
@@ -20,3 +20,22 @@ int main(){
         }
     return 0;
 }
+//-----------------------------------------------------------
+//find the missing number in the array
+//optimal solution 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] = {8,1,2,3,7,4,5};
+    int sum = 0;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0; i < n; i++){
+        sum = sum + arr[i];
+    }
+    int t= n+1;
+    long long o_sum = (t *(t+1))/2;
+    
+    cout<<o_sum -sum;
+    return 0;
+}
