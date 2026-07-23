fa2a9b2a6cb66e4ffa64d69a5fccce6c8aea7476
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-23 20:15:37 +0530

    Update Find the number that appears once.cpp

--- Array/Find the number that appears once.cpp
+++ Array/Find the number that appears once.cpp
@@ -24,3 +24,27 @@ int main(){
     cout<<result;
     return 0;
 }
+
+
+
+//-------optimal way
+#include<bits/stdc++.h>
+using namespace std;
+
+int app_num(int arr[],int n){
+    
+    int xor1 = 0;
+    for(int i = 0; i < n; i++){
+        xor1 = xor1  ^ arr[i];
+    }
+    
+    return xor1;
+}
+
+int main(){
+    int arr[] = {4,1,2,1,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int result = app_num(arr,n);
+    cout<<result;
+    return 0;
+}
