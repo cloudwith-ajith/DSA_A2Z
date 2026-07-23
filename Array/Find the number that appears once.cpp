c9b415fcc67a185ca4bd2876554f49e89139be08
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-23 20:14:58 +0530

    Create Find the number that appears once.cpp

---
+++ Array/Find the number that appears once.cpp
@@ -0,0 +1,26 @@
+// code for the  Find the number that appears once, and the other numbers twice
+// better method 
+#include<bits/stdc++.h>
+using namespace std;
+
+int find_num(int arr[],int n){
+    map <int,int> mpp;
+    for(int i = 0; i < n;i++){
+        mpp[arr[i]] = mpp[arr[i]] + 1;
+    }
+    
+    for(int i = 0; i < n; i++){
+        if(mpp[arr[i]] == 1){
+            return arr[i];
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {4,1,2,1,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int result = find_num(arr,n);
+    cout<<result;
+    return 0;
+}
