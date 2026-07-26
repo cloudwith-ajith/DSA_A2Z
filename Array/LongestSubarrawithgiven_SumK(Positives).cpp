a17d6b39e6fb7d2a0b5250f86dbefa5d9da9c10c
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-26 19:36:29 +0530

    Add function to find longest subarray with sum k
    Implement a function to find the longest subarray with a given sum k for positive integers.

---
+++ Array/LongestSubarrawithgiven_SumK(Positives).cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longest_subarray(int arr[],int n,int k){
+    unordered_map <int,int> prefix_mpp;
+    int max_len = 0;
+    int sum = 0;
+    for(int i = 0;i < n; i++){
+        sum = sum + arr[i];
+        //find the very first len of the k
+        if (sum == k ){
+            max_len = i + 1;
+        }
+        //find the key wheather present in the prefixmapp
+        if(prefix_mpp.find(sum - k) != prefix_mpp.end()){
+            max_len = max(max_len, i - prefix_mpp[sum-k]);
+        }
+        // store the memory of the element 
+        if(prefix_mpp.find(sum) == prefix_mpp.end()){
+            prefix_mpp[sum] = i;
+        }
+    }
+    
+    return max_len;
+} 
+
+int main(){
+    int arr[] = {10, 5, 2, 7, 1, 9};
+    int k = 15;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int result = longest_subarray(arr,n,k);
+    cout<<result;
+    return 0;
+}
