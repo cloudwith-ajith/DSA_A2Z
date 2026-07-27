7650eca6c76e3e7be42bfc6db7364368f414a53a
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-27 19:37:16 +0530

    Update 10to14.cpp

--- Array/Revesion/10to14.cpp
+++ Array/Revesion/10to14.cpp
@@ -85,3 +85,29 @@ int main(){
     cout<<result;
     return 0;
 }
+//-------------The optimal way to find the missing number
+// TC O(n)
+// sp O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+int missing_number(int arr[],int n ){
+    //using XOR
+    int xor1 = 0;
+    int xor2 = 0;
+    for(int i = 0; i < n; i++){
+        xor2 = xor2 ^ arr[i];
+        xor1 = xor1 ^ (i +1);
+    }
+    xor1 = xor1 ^ (n+1);
+    int miss = xor1 ^ xor2;
+    return miss;
+}
+
+int main(){
+    int arr[] = {8, 2, 4, 5, 3, 7, 1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int result = missing_number(arr,n);
+    cout<<result;
+    return 0;
+}
