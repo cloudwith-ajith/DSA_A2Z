e3ed815d4343ea3d122fd7b3e25dbc76a9ed600f
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-07-29 19:56:37 +0530

    Create Two_sum.cpp

---
+++ Array/Medium/Two_sum.cpp
@@ -0,0 +1,32 @@
+//Two_sum---->google
+//-----------------------brute force
+#include<bits/stdc++.h>
+using namespace std;
+
+bool two_sum(int arr[],int n,int target){
+    
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] + arr[j] == target){
+                return true;
+            }
+        }
+    }
+    
+    return false;
+    
+}
+
+int main(){
+    int arr[] = {2,6,5,8,11};
+    int n  = sizeof(arr)/sizeof(arr[0]);
+    int target = 14;
+    bool result = two_sum(arr,n,target);
+    if(result == true){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
+    return 0;
+}
