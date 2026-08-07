993f5cc3f45e07f7ab154427d1f239b9704c5fad
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-07 23:47:50 +0530

    Update Sort an array of 0's _1's _and _2's.cpp

--- Array/Medium/Sort an array of 0's _1's _and _2's.cpp
+++ Array/Medium/Sort an array of 0's _1's _and _2's.cpp
@@ -3,7 +3,7 @@
 #include<bits/stdc++.h>    
 using namespace std;
 void sort_sort(int arr[],int n){
-    // create the temp array  
+    // create the temp array     
     vector <int> temp;        
     // first loop for the 0 to 2 element   
     for(int i = 0 ; i < 3; i++){      
