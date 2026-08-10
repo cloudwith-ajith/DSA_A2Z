cfa737c5d0a0146381fe84698083d2b658c1d53e
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-10 22:28:39 +0530

    Update 1 to 3.cpp

--- Array/Medium/Revesion/1 to 3.cpp
+++ Array/Medium/Revesion/1 to 3.cpp
@@ -26,3 +26,34 @@ int main(){
     cout<< res;
     return 0;
 }
+
+///----using the hash map 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string two_sum(vector<int> arr,int target){
+    int n  = arr.size();
+    unordered_map <int,int> mpp;
+    
+    for(int i = 0 ; i < n; i++){
+        int a = target - arr[i];
+        
+        if(mpp.find(a) != mpp.end()){
+            return "yes";
+        }
+        if(mpp.find(a) == mpp.end()){
+            mpp[arr[i]] = i;
+        }
+    }
+    return "no";
+}
+
+
+int main(){
+    vector <int> arr = {2,6,5,8,11};
+    int target = 14;
+    string res = two_sum(arr,target);
+    cout<<res;
+    return 0;
+}
