e814330e2e1471bd95feff66d4b9212abbe909d9
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-10 22:29:39 +0530

    Update 1 to 3.cpp

--- Array/Medium/Revesion/1 to 3.cpp
+++ Array/Medium/Revesion/1 to 3.cpp
@@ -94,5 +94,49 @@ int main(){
     return 0 ;
 }
 
+////////sort 0 1 2 in the array 
+#include<bits/stdc++.h>
+using namespace std;
+
+void sot(int arr[],int n){
+    int count0 = 0;
+    int count1 = 0;
+    int count2 = 0;
+    
+    for(int i = 0 ; i < n ; i++){
+        if(arr[i] == 0 ){
+            count0++;
+        }else if(arr[i] == 1){
+            count1++;
+        }else{
+            count2++;
+        }
+    }
+    
+    for(int i = 0 ; i < count0; i++){
+        arr[i] = 0;
+    }
+    
+    for(int i = count0 ; i < count0+count1; i++){
+        arr[i] = 1;
+    }
+    
+    
+    for(int i = count0+count1 ; i < n; i++){
+        arr[i] = 2;
+    }
+}
+
+
+int main(){
+    int arr[] = {1, 0, 2, 1, 0};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    sot(arr,n);
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
+
 
 
