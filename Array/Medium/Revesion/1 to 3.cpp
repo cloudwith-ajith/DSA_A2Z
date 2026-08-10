17e428d48b04ca772dfa9b368c67be83dba623d9
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-10 22:29:15 +0530

    Add optimal two_sum function with two pointers
    Implemented an optimal two_sum function using two pointers after sorting the array.

--- Array/Medium/Revesion/1 to 3.cpp
+++ Array/Medium/Revesion/1 to 3.cpp
@@ -57,3 +57,42 @@ int main(){
     cout<<res;
     return 0;
 }
+
+//-----------------------optimal way
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string two_sum(vector<int> arr,int target){
+    int n = arr.size();
+    int right = n-1;
+    int left  = 0 ;
+    //sort the array 
+    sort(arr.begin(),arr.end());
+    while(left < right){
+        int sum = 0;
+        sum = arr[left] +arr[right];
+        
+        if(sum > target){
+            right--;
+        }
+        if(sum < target){
+            left++;
+        }
+        if(sum == target){
+            return "yes";
+        }
+    }
+    return "no";
+}
+
+int main(){
+    vector <int> arr = {2,6,5,8,11};
+    int target = 14;
+    string res = two_sum(arr,target);
+    cout<<res;
+    return 0 ;
+}
+
+
+
