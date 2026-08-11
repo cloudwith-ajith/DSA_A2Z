85e070dfaad2640f83798473b93b0c94a78804b0
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-11 22:52:00 +0530

    Add greedy algorithm for stock buy and sell problem
    Implemented a greedy algorithm to find the maximum profit from stock prices by tracking minimum buy price and maximum profit in a single loop.

--- Array/Medium/Stock Buy and Sell.cpp
+++ Array/Medium/Stock Buy and Sell.cpp
@@ -27,3 +27,35 @@ int main(){
 
 // the time complexity of the solution is O(2n)
 // the space complexity of the solution is O(1)
+
+
+//optimal way ----------------greedy algo 
+#include<bits/stdc++.h>
+using namespace std;
+
+int stock(int arr[], int n){
+    int max_element = 0;
+    int min_element  = INT_MAX;
+    //single loop 
+    for(int i = 0 ; i < n; i++){
+        if(arr[i] < min_element){
+            min_element = arr[i];
+        }
+        max_element = max(max_element,arr[i] - min_element);
+    }
+
+    return max_element;
+
+}
+
+
+
+int main(){
+    int arr[] = {7,1,5,3,6,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int result = stock(arr,n);
+    cout<< result;
+    return 0;
+}
+//the time complexity od the solution O(n)
+//the space complexityof the solution O(1)
