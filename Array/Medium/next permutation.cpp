2ec3da5e604f46318d832bbb3c376b5eb3946a62
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-12 22:33:28 +0530

    Add next_permutitation function for permutations
    Implemented a function to find the next permutation of a given vector using STL's next_permutation.

--- Array/Medium/next permutation.cpp
+++ Array/Medium/next permutation.cpp
@@ -12,3 +12,40 @@ int main(){
     }
     return 0;
 }
+
+//brute force method
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector <int> next_permutitation(vector <int> arr){
+    //create the vector to store the permutation element
+    vector <vector <int>> temp;
+    vector<int> original_arr = arr;
+    //sort the given array to do permutation 
+    sort(arr.begin(),arr.end());
+    //create the combination using the cpp stl
+    //next_permutation
+    do{
+        temp.push_back(arr);
+    }while(next_permutation(arr.begin(),arr.end()));
+    //return the next permutation 
+    for(int i = 0 ; i < temp.size();i++){
+        if(temp[i] == original_arr){
+            if(i == temp.size() - 1){
+                return temp[0];
+            }
+            return temp[i+1];
+        }
+    }
+    return original_arr;
+}
+
+int main(){
+    vector <int> arr = {1,3,2};
+    vector result = next_permutitation(arr);
+    for(int i : result){
+        cout<<i<<" ";
+    }
+    return 0;
+}
