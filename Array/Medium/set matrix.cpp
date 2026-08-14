71b9d6ede5f5fffd8e484e4cecc65a6660816a50
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-14 23:38:31 +0530

    Update set matrix.cpp

--- Array/Medium/set matrix.cpp
+++ Array/Medium/set matrix.cpp
@@ -46,3 +46,49 @@ int main(){
     }
     return 0;
 }
+
+///----------------------- the better solution for the problem 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void set_matrix(vector<vector<int>> &arr){
+    int n = arr.size();
+    int m = arr[0].size();
+
+    vector<int> row_arr(n,0);
+    vector<int> col_arr(m,0);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(arr[i][j] == 0){
+                row_arr[i] = 1;
+                col_arr[j] = 1;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(row_arr[i] == 1 || col_arr[j] == 1){
+                arr[i][j] = 0;
+            }
+        }
+    }
+}
+
+
+
+int main(){
+    vector<vector<int>> arr = {{1,1,1},{1,0,1},{1,1,1}};
+    set_matrix(arr);
+    for(auto i : arr){
+        for(auto j : i){
+            cout<<j<<" ";
+        }cout<<endl;
+    }
+    return 0;
+}
+
+// Time Complexity: O(n*m)
+// Space Complexity: O(n+m)
