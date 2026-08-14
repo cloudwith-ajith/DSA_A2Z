363fa7e3d5792bebcb904291e74aa15e46cbc61e
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-14 23:37:20 +0530

    Create set matrix.cpp

---
+++ Array/Medium/set matrix.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void set_matrix(vector<vector<int>> &arr){
+    int n = arr.size();
+    int m = arr[0].size();
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(arr[i][j] == 0){
+                for(int row = 0; row < n; row++){
+                    if(arr[row][j] != 0){
+                        arr[row][j] = -1;
+                    }
+                }
+
+                for(int col = 0; col < m; col++){
+                    if(arr[i][col] != 0){
+                        arr[i][col] = -1;
+                    }
+                }
+            }
+        }
+    }
+
+    // reset the -1 into the 0
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(arr[i][j] == -1){
+                arr[i][j] = 0;
+            } 
+        }
+    }
+}
+
+
+
+int main(){
+    vector<vector<int>> arr = {{1,1,1},{1,0,1},{1,1,1}};
+    set_matrix(arr);
+    for(auto i : arr){
+        for(auto j : i){
+            cout<<j<<" ";
+        }cout<<endl;
+    }
+    return 0;
+}
