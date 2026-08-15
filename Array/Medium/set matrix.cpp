89700ad9ed71ebcce5f3e38fbe00bbbf9e7aabcf
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-15 22:35:51 +0530

    Update set matrix.cpp

--- Array/Medium/set matrix.cpp
+++ Array/Medium/set matrix.cpp
@@ -90,3 +90,64 @@ int main(){
 
 // Time Complexity: O(n*m)
 // Space Complexity: O(n+m)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void set_matrix(vector <vector<int>> &arr){
+    //row
+    int n = arr.size();
+    int m = arr[0].size();
+    int col0 = 1;
+    for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+
+        if(arr[i][j] == 0){
+
+            arr[i][0] = 0;
+
+            if(j == 0){
+                col0 = 0;
+            }
+            else{
+                arr[0][j] = 0;
+            }
+        }
+    }
+}
+ 
+
+    for(int i = 1; i < n; i++){
+        for(int j = 1; j < m; j++){
+            if(arr[i][0] == 0 || arr[0][j]== 0 ){
+                arr[i][j] = 0;
+            }
+        }
+    }
+    //asign zero for the first rwo
+    if(arr[0][0] == 0){
+        for(int j = 0; j < m; j++){
+            arr[0][j] = 0;
+        }
+    }
+    //for column
+    if(col0 == 0){
+        for(int i = 0; i < n; i++){
+            arr[i][0] = 0;
+        }
+    }
+}
+
+
+
+
+int main(){
+    vector <vector<int>> arr = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+    set_matrix(arr);
+    for(auto i : arr){
+        for(auto j : i){
+            cout<<j<<" ";
+        }cout<<endl;
+    }
+    return 0;
+}
