fad61e8476fc7f24c8a82b21b45f61534fa639be
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-16 14:55:22 +0530

    Update Rotate 90 degree.cpp

--- Array/Medium/Rotate 90 degree.cpp
+++ Array/Medium/Rotate 90 degree.cpp
@@ -26,3 +26,44 @@ int main(){
     rotate_matrix(arr);    
     return 0;
 }
+
+
+///--------------ooptimal way to write the code 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotate90(vector<vector<int>> &arr){
+    int n =arr.size();
+    //traversal
+    for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            swap(arr[i][j],arr[j][i]);
+        }
+    }
+    // reverse the row to get the correct degree
+    for(int i = 0; i < n; i++){
+        reverse(arr[i].begin(),arr[i].end());
+    }
+
+    for(auto i : arr){
+        for(auto j : i){
+            cout<<j<<" ";
+        }cout<<endl;
+    }
+}
+
+
+int main(){
+    vector<vector<int>> arr = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    rotate90(arr);
+    return 0;
+}
+
+// time complexity O(n*n+n) ---> O(n*n)
+// space complexity O(1)
