a93334f99b65dfcdc910153eefc1aa50e122719f
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-17 22:22:47 +0530

    Optimize count_subarray function using hash map
    Optimized the count_subarray function to use a hash map for O(n) complexity.

--- Array/Medium/Count subarrays with given sum.cpp
+++ Array/Medium/Count subarrays with given sum.cpp
@@ -30,3 +30,42 @@ int main(){
     cout<<result;
     return 0;
 }
+
+
+/// we can optimal the code into the O(n)
+// using the hash map 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int count_subarray(vector <int> &arr,int k){
+    int n = arr.size();
+    unordered_map <int,int> mpp;
+    //base case for
+    mpp[0] = 1;
+    int prefix_sum = 0;
+    int count = 0;
+    for(int i = 0 ; i < n; i++){
+        // sum of the all element before element
+        prefix_sum = prefix_sum + arr[i];
+        //element to check 
+        int remove = prefix_sum - k;
+        //check the k- orefix in the map
+        if(mpp.find(remove) != mpp.end()){
+            count++;
+        }
+        //update the element tot the map 
+        mpp[prefix_sum]++;
+    }
+
+    return count;
+}
+
+
+int main(){
+    vector <int> arr = {1,2,3};
+    int k = 3;
+    int result = count_subarray(arr,k);
+    cout<<result;
+    return 0;
+}
