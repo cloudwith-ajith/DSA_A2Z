d87b660f8108fb844224ced60425a7218a9545eb
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-18 22:53:48 +0530

    Add majority element finder using Boyer-Moore algorithm
    Implement Boyer-Moore Voting Algorithm to find majority element.

--- Array/Medium/Revesion/majority.cpp
+++ Array/Medium/Revesion/majority.cpp
@@ -60,3 +60,51 @@ int main(){
 
 //----- the time compleity of the code is O(n)
 // the space complexity of the code is O(n) because of the hash map 
+
+
+//------------------optimal way 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int majorty(vector<int> &arr){
+    int n = arr.size();
+    int counter = 0;
+    int voter = 0;
+
+    for(int i = 0; i < n; i++){
+        if(counter == 0){
+            voter = arr[i];
+            counter++;
+        }else if(voter == arr[i]){
+            counter++;
+        }else{
+            counter--;
+        }
+    }
+    int check  = 0;
+    for(int i = 0; i < n; i++){
+        if(voter == arr[i]){
+            check++;
+        }
+    }
+    if(check > n/2){
+        return voter;
+    }
+
+    return -1;
+
+}
+
+
+int main(){
+    vector <int> arr = {7, 0, 0, 1, 7, 7, 2, 7, 7};
+    int result = majorty(arr);
+    cout<<result;
+    return 0;
+}
+
+
+// the time compleity of the problem is O(n);
+// the space coomplexity of the code is O(1);
