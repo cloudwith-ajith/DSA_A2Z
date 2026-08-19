c33109d97c439f3c8314126398272abeb12dcb9a
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-19 22:07:32 +0530

    Update balance.cpp

--- Array/Medium/Revesion/balance.cpp
+++ Array/Medium/Revesion/balance.cpp
@@ -225,4 +225,66 @@ int main(){
     return 0;
 }
 
+//--------------------spiral problem
 
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> spiral(vector<vector<int>> &arr){
+    int n = arr.size();
+    int m = arr[0].size();
+    vector <int> temp;
+    int top = 0;
+    int left = 0;
+    int right = m-1;
+    int bottom = n-1;
+
+    while(left <= right && top <= bottom){
+        
+        for(int i = left; i <= right; i++){
+            temp.push_back(arr[top][i]);
+        }
+        
+        top++;
+        
+        for(int i = top; i <= bottom; i++){
+            temp.push_back(arr[i][right]);
+        }
+        
+        right--;
+        
+        if(top <= bottom){
+            for(int i = right; i >= left; i--){
+                temp.push_back(arr[bottom][i]);
+            }
+            bottom--;
+        }
+
+        
+
+        if(left <= right){
+            for(int i = bottom; i >= top; i-- ){
+                temp.push_back(arr[i][left]);
+            }
+            left++;
+        }
+
+        
+    }
+
+    return temp;
+}
+
+int main(){
+    vector<vector<int>> arr = {
+        { 1, 2, 3, 4 },
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 },
+        { 13, 14, 15, 16 }
+    };
+    vector<int> result = spiral(arr);
+    for(auto i : result){
+        cout<<i<<" ";
+    }
+    return 0;
+}
