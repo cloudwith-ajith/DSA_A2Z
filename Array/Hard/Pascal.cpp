c240bcaf693b753345aeaf9554e042e3118a9d14
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-23 23:41:03 +0530

    Fix formatting of variable declaration in Pascal.cpp

--- Array/Hard/Pascal.cpp
+++ Array/Hard/Pascal.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int position(int n, int col){  
-    long long ans = 1;   
+    long long ans = 1;    
     for(int i = 0; i < col; i++){
         ans = ans * (n-i);  
         ans = ans / (i+1);
