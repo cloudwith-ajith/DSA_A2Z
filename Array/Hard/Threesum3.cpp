975a654437bed74add7f9dbcd31fd744528af720
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-25 23:06:44 +0530

    Add three sum function to find triplets summing to zero
    Implemented a function to find all unique triplets in an array that sum to zero. Updated main function to demonstrate usage.

--- Array/Hard/Threesum3.cpp
+++ Array/Hard/Threesum3.cpp
@@ -70,3 +70,65 @@ int main(){
 
 // the time complexity of the code is O(2^n)
 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> threesum(vector<int> &arr){
+    int n = arr.size();
+    vector<vector<int>> result;
+    //sort the array 
+    sort(arr.begin(),arr.end());
+    //outer loop 
+    for(int i = 0; i < n; i++){
+        if(i > 0 && arr[i] == arr[i-1]){
+            continue;
+        }
+
+        int j = i + 1;
+        int k = n - 1;
+        while(j < k){
+            int sum = arr[i]+arr[j]+arr[k];
+            // if the summ is greater than the zero 
+            if(sum > 0){
+                k--;
+            }else if(sum < 0){
+                j++;
+            }else{
+                vector<int> temp = {arr[i],arr[j],arr[k]};
+                result.push_back(temp);
+                j++;
+                k--;
+                // to remove the duplicate 
+                while(j < k && arr[j] == arr[j-1]){
+                    j++;
+                }
+                while(j < k && arr[k] == arr[k+1]){
+                    k--;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+
+int main(){
+    vector <int> arr = {-1,0,1,2,-1,-4};
+    vector<vector<int >> result = threesum(arr);
+    for(auto i : result){
+        for(auto j : i){
+            cout<<j<<" ";
+        }cout<<endl;
+    }
+    return 0;
+}
+
+// optimal way foe the three sum 
+// the time complexity for the code is O(n^2)
+// the space complexity for the code is O(1), the vector space is calculate 
+// as the ouptut space 
+
+
