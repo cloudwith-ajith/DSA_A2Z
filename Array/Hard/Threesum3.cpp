c45980de66e125e864a49525bab361393d7de129
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-25 23:06:20 +0530

    Add optimized 3Sum solution using sets
    Implemented a more efficient method to solve the 3Sum problem using sets to avoid duplicates.

--- Array/Hard/Threesum3.cpp
+++ Array/Hard/Threesum3.cpp
@@ -32,3 +32,41 @@ int main(){
     }
     return 0;
 }
+//-------------//// better method to solve the THREESUM 3sum 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> threesum(vector<int> &arr){
+    int n = arr.size();
+    set <vector<int>> tempset;
+    for(int i = 0; i < n; i++){
+        set <int> eleset;
+        for(int j = i + 1; j < n; j++){
+            int k = -(arr[i] + arr[j]);
+            if(eleset.find(k) != eleset.end()){
+                vector<int> tp = {arr[i],arr[j],k};
+                sort(tp.begin(),tp.end());
+                tempset.insert(tp);
+            }
+            eleset.insert(arr[j]);
+        }
+    }
+    vector<vector<int>> result(tempset.begin(),tempset.end());
+    return result;
+}
+
+
+int main(){
+    vector <int> arr = {-1,0,1,2,-1,-4};
+    vector<vector<int >> result = threesum(arr);
+    for(auto i : result){
+        for(auto j : i){
+            cout<<j<<" ";
+        }cout<<endl;
+    }
+    return 0;
+}
+
+// the time complexity of the code is O(2^n)
+
