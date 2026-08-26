0d010e354b7d101b9dc934ddde2055491d43ff87
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-26 20:58:08 +0530

    Add function to find longest subarray length
    Implement function to find longest subarray with given sum.

---
+++ Array/Hard/Longestsubarray length.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lonlen(vector<int> &arr, int target){
+    int n = arr.size();
+    unordered_map <int,int> mpp;
+    // max lent 
+    int maxlen = 0;
+    int sum = 0 ;
+    for(int i = 0 ; i < n; i++){
+        // add the sum
+        sum += arr[i];
+        // the sum equal to the target 
+        // then the len from the zero to is a subarray
+        if(sum == target){
+            maxlen = i + 1;
+        }else if(mpp.find(sum) != mpp.end()){                
+             maxlen = max(maxlen, i - mpp[sum]);
+        }else{
+                // the condition is the prefix is not in the array then add
+                mpp[sum] = i;
+            }
+        }
+    //return the maxlen
+    return maxlen;
+}
+
+
+int main(){
+    vector<int> arr = {9, -3, 3, -1, 6, -5};
+    int target = 0 ;
+    int result = lonlen(arr,target);
+    cout<<result;
+    return 0;
+}
