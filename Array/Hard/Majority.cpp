9ab5aa2dda5768a10af60c2568ea71e3b0b2abbf
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-08-30 23:43:53 +0530

    Update Majority.cpp

--- Array/Hard/Majority.cpp
+++ Array/Hard/Majority.cpp
@@ -8,7 +8,7 @@ vector <int> majority(vector <int> &arr){
     int cnt1 = 0;
     int cnt2 = 0; 
     int el1 = INT_MIN;   
-    int el2 = INT_MIN; 
+    int el2 = INT_MIN;   
 
     for(int i = 0; i <  n; i++){
         if(cnt1 == 0 && el2 != arr[i]){
