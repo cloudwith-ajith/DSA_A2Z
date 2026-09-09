24a236ce01ded5b18f16e719521e44d6e5d5039e
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-09 14:16:14 +0530

    Update Find Repeating and Missing.cpp

--- Array/Hard/Find Repeating and Missing.cpp
+++ Array/Hard/Find Repeating and Missing.cpp
@@ -83,3 +83,38 @@ int main(){
 //------------using the hashing
 // The Time complexity of the code is O(n)
 // The Space complexity of the code is O(n)
+
+///----optimal solution 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector <int> ReMs(vector<int> &arr){
+    int n = arr.size();
+    int sn1 = (n*(n+1))/2;
+    int sn2 = (n*(n+1) * (2 * n + 1))/6;
+    int s1 = 0;
+    int s2 = 0;
+    for(int i = 0; i < n; i++){
+        s1 += arr[i];
+        s2 = s2 + (arr[i] * arr[i]);
+    }
+    int var1 = s1 - sn1;
+    int var2 = s2 - sn2;
+    var2 = var2 / var1;
+    int x =  (var1 + var2) / 2;
+    int y =  x - var1;
+    vector <int> res  = {x,y};
+    return res;
+}
+
+int main(){
+    vector<int> arr = {3, 1, 2, 5, 4, 6, 7, 5};
+    vector<int> result = ReMs(arr);
+    for(auto i : result){
+        cout<<i<<" ";
+    }
+    return 0;
+}
+// the timecomplexity of the code is O(n);
+// the space complexity of the code is O(1);
