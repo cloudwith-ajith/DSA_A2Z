5677538e8643d79df46b41154ecf2bfa484d2253
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-12 00:17:56 +0530

    Create CountInversive.cpp

---
+++ Array/Hard/CountInversive.cpp
@@ -0,0 +1,25 @@
+// brute force method for the problem
+#include<bits/stdc++.h>
+using namespace std;
+
+int CountInver(vector <int> &arr){
+    int n = arr.size();
+    int counter = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] > arr[j]){
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+
+int main(){
+    vector <int> arr = {5,3,2,1,4};
+    int res = CountInver(arr);
+    cout<<res;
+    return 0;
+}
+// The Time complexity of the code is O(n^2)
+// The Space complexity of the code is O(n)
