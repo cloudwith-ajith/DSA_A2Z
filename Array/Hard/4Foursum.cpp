466dd783ddca01339f4eb7d8eb587927fd3c9d0a
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-13 23:33:38 +0530

    Update 4Foursum.cpp

--- Array/Hard/4Foursum.cpp
+++ Array/Hard/4Foursum.cpp
@@ -6,7 +6,7 @@ vector<vector<int>> foursum(vector<int> &arr,int target){
     vector<vector<int>> ans;
     sort(arr.begin(),arr.end());
     for(int i = 0; i < n; i++){      
-        if(i > 0 && arr[i] == arr[i-1]){
+        if(i > 0 && arr[i] == arr[i-1]){  
             continue;     
         }   
         for(int j = i + 1; j < n; j++){
