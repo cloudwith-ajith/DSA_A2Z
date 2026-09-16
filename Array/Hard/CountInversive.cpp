15f5863d9df7f19277b03f198ae8b264c5eeb3b6
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-16 23:19:04 +0530

    Update CountInversive.cpp

--- Array/Hard/CountInversive.cpp
+++ Array/Hard/CountInversive.cpp
@@ -7,7 +7,7 @@ int CountInver(vector <int> &arr){
     int counter = 0; 
     for(int i = 0; i < n; i++){
         for(int j = i + 1; j < n; j++){
-            if(arr[i] > arr[j]){
+            if(arr[i] > arr[j]){ 
                 counter++;
             }    
         }
