fe8482b26a8b88cdda3abd44a6c4c6fbff54121a
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-20 21:43:51 +0530

    Create Maximum Product Subarray.cpp

---
+++ Array/Hard/Maximum Product Subarray.cpp
@@ -0,0 +1,29 @@
+// breute force method 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxproductt(vector <int> &arr){
+    int n = arr.size();
+    int maxpro = INT_MIN;
+    for(int i = 0; i < n; i++){
+        int temp = arr[i];
+        for(int j = i + 1; j < n; j++){
+            temp = temp * arr[j];
+            maxpro = max(maxpro, temp);
+        }
+    }
+    return maxpro;
+}
+
+int main(){
+    vector <int> arr ={2,3,-2,4};
+    int result = maxproductt(arr);
+    cout<<result;
+    return 0;
+}
+
+// The Time complexity of the code is O(n^2)
+// The Space complexity of the code is O(1)
+
+
