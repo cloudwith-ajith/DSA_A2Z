52a7bf3b68dc89f415ceadf1be5e60c93889e0c6
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-20 21:53:37 +0530

    Update Maximum Product Subarray.cpp

--- Array/Hard/Maximum Product Subarray.cpp
+++ Array/Hard/Maximum Product Subarray.cpp
@@ -28,7 +28,36 @@ int main(){
 //--------------------------------------------------------------------------
 // The optimal solution 
 
+#include<bits/stdc++.h>
+using namespace std;
 
+int MaxProds(vector <int> &arr){
+    int n = arr.size();
+    int maxprod = INT_MIN;
+    int prefix = 1;
+    int sufix = 1;
+    for(int i = 0; i < n; i++){
+        if(prefix == 0) prefix = 1;
+        if(sufix == 0) sufix = 1;
+
+        prefix = prefix * arr[i];
+        sufix  = sufix * arr[n - i -1];
+
+        maxprod = max(maxprod,max(prefix,sufix));
+    }
+
+    return maxprod;
+}
+
+int main(){
+    vector <int> arr = {2,3,-2,4};
+    int result = MaxProds(arr);
+    cout<<result;
+    return 0;
+}
+
+// The Time complexity of the code is O(n)
+// The Space complexity of the code is O(1)
 
 
 
