d5c22e6fe9524c18807775da1680a52c5a8fa70b
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-21 19:28:17 +0530

    Add binary search implementation in C++
    Implemented binary search algorithm to find an element in a sorted array. Updated time complexity to O(log n) and corrected low index increment.

--- Binary Search/Search a Sorted Array.cpp
+++ Binary Search/Search a Sorted Array.cpp
@@ -25,3 +25,48 @@ int main(){
 
 // The Time complexity of the code is O(n)
 // The Space complexity of the code is O(1)
+
+
+// binary search 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarysearch(vector <int> &arr,int x){
+    int n = arr.size();
+    //the first index as the low 
+    int low  = 0 ;
+    // the last index as the high 
+    int high = n - 1;
+    //while loop 
+    while(low <= high){
+        // get the mid value
+        int mid = (low + high) / 2;
+        // if the mid value is the target value, then return the index
+        if(arr[mid] == x){
+            return mid;
+        }
+        // if the mid value is greater, change the high 
+        if(arr[mid] > x){
+            high = mid -1;
+        }
+        // if the mid value is lesser than, change the low value 
+        else{
+            low = mid + 1;
+        }
+    }
+    // return -1 if there is no target found 
+    return -1;
+}
+
+
+int main(){
+    vector <int> arr = {1, 3, 5, 7, 9, 11};
+    int x = 7;
+    int result = binarysearch(arr,x);
+    cout<<result;
+    return 0;
+}
+
+// The Time complexity of the code is O(log n)
+// The Space complexity of the code is O(1)
