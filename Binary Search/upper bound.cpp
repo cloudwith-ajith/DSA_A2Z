38a639655949bfea3d7e09b963ca5528380d2f7b
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-22 17:23:05 +0530

    Create upper bound.cpp

---
+++ Binary Search/upper bound.cpp
@@ -0,0 +1,34 @@
+//----------------------------------------------------------------uuper bound 
+// -----------------condition
+// it should the first greater than element from the target 
+// even the target is present in the array we need to take the first greater number 
+
+
+//########################################################################
+// brute force method for the upper bound 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int upper_bound(vector <int> &arr, int target){
+    int n = arr.size();
+    for(int i = 0 ; i < n; i ++){
+        if( arr[i] > target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector <int> arr = {1, 3, 5, 5, 5, 9};
+    int target = 5;
+    int result = upper_bound(arr,target);
+    cout<< result;
+    return 0;
+}
+
+// the time complexity of the code is O(n)
+// the space complexity of the code is O(1)
+
+//########################################################################
