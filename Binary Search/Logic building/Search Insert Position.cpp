1bc5d7d490594eccdb2c119c4ac72a9b15cdd9cd
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-22 18:19:12 +0530

    Update Search Insert Position.cpp

--- Binary Search/Logic building/Search Insert Position.cpp
+++ Binary Search/Logic building/Search Insert Position.cpp
@@ -1,1 +1,36 @@
-1, 3, 5, 6
+// Optimal code for the search insert position
+#include<bits/stdc++.h>
+using namespace std;
+
+int lowerbound(vector <int> &arr, int target){
+    int n = arr.size();
+    int answer = n;
+    int low = 0;
+    int high = n - 1;
+
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] >= target){
+            answer = mid;
+            high = mid - 1;
+        }else{
+            low = mid + 1; 
+        }
+    }
+
+    return answer;
+}
+
+
+
+int main(){
+    vector <int> arr = {1, 3, 5, 6};
+    int target = 2;
+    int result = lowerbound(arr, target);
+    cout<< result;
+    return 0;
+}
+
+// the time complexity of the code is O(log n)
+// the space complexity of the code is O(1)
