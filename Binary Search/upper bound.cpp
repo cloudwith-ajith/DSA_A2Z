5d51453a9bcced62095bbf6bab7a871f99de620e
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-22 17:39:34 +0530

    Update upper bound.cpp

--- Binary Search/upper bound.cpp
+++ Binary Search/upper bound.cpp
@@ -32,3 +32,42 @@ int main(){
 // the space complexity of the code is O(1)
 
 //########################################################################
+
+// Optimal code for the upper bound 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int lowerbound(vector <int> &arr, int target){
+    int n = arr.size();
+    int answer = n;
+    int low = 0;
+    int high = n - 1;
+
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] > target){
+            answer = mid;
+            high = mid - 1;
+        }else{
+            low = mid + 1; 
+        }
+    }
+
+    return answer;
+}
+
+
+
+int main(){
+    vector <int> arr = {1, 3, 5, 7, 9, 11};
+    int target = 5;
+    int result = lowerbound(arr, target);
+    cout<< result;
+    return 0;
+}
+
+// the time complexity of the code is O(log n)
+// the space complexity of the code is O(1)
