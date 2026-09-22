2c5f8317d459c7d21673267b75b47ddd179b99b0
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-22 18:20:21 +0530

    Update Search Insert Position.cpp

--- Binary Search/Logic building/Search Insert Position.cpp
+++ Binary Search/Logic building/Search Insert Position.cpp
@@ -1,3 +1,31 @@
+// brute force method for the search insert position 
+#include<bits/stdc++.h>
+using namespace std;
+
+int upper_bound(vector <int> &arr, int target){
+    int n = arr.size();
+    for(int i = 0 ; i < n; i ++){
+        if(arr[i] == target || arr[i] > target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector <int> arr = {1,3,5,7,9,11};
+    int target = 6;
+    int result = upper_bound(arr,target);
+    cout<< result;
+    return 0;
+}
+
+
+// the time complexity of the code is O(n)
+// hte space complexity of the code is O(1)
+
+
+
 // Optimal code for the search insert position
 #include<bits/stdc++.h>
 using namespace std;
@@ -21,8 +49,6 @@ int lowerbound(vector <int> &arr, int target){
 
     return answer;
 }
-
-
 
 int main(){
     vector <int> arr = {1, 3, 5, 6};
