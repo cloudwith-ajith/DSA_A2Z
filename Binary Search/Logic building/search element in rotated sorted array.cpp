5f8b083dba30ab1b7ce04695199c747563fcfa01
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-23 19:22:56 +0530

    Create search element in rotated sorted array.cpp

---
+++ Binary Search/Logic building/search element in rotated sorted array.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int searchinrotate(vector <int> &arr, int target){
+    int n = arr.size();
+    int low = 0;
+    int high  = n - 1;
+
+    while(low <= high){
+        int mid = (low + high) / 2;
+
+        if(arr[mid] == target){
+            return mid;
+        }
+        if(arr[low] < arr[mid]){
+            if(arr[low] <= target && target <= arr[mid]){
+                high = mid -1;
+            }else{
+                low = mid +1;
+            }
+        }else{
+            if(arr[mid] <= target && arr[high] >= target){
+                low = mid + 1;
+            }else{
+                high = mid -1;
+            }
+        }
+    }
+    
+    return -1;
+}
+
+int main(){
+    vector <int> arr = {4,5,6,7,0,1,2};
+    int target = 0;
+    int result = searchinrotate(arr,target);
+    cout<<result;
+    return 0;
+}
