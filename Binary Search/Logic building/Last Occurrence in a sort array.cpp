8a1a8dd926e48e64a26baa33bba4d3a0f0141426
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-23 17:18:35 +0530

    Update Last Occurrence in a sort array.cpp

--- Binary Search/Logic building/Last Occurrence in a sort array.cpp
+++ Binary Search/Logic building/Last Occurrence in a sort array.cpp
@@ -28,3 +28,53 @@ int main(){
 // the space complexity of the code is O(1)
 
 //-----------------------optimal way for the problem---------
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int LastOccurrence(vector <int> &arr, int target){
+    int n = arr.size();
+    // the low point to the first index of the array 
+    int low = 0;
+    // the high point to the last index of the array 
+    int high = n - 1;
+    // the answer variable used to return the answer
+    int answer = -1;
+    // the while loop for the binary search 
+    while(low <= high){
+        // find the mid element 
+        int mid = (high + low) / 2;
+        // if the element is equal to the target, mark the answer to the mid index
+        if(arr[mid] == target){
+            answer = mid;
+            // used to move the index always to the right 
+            // becasue always at the right of the array 
+            low = mid + 1;
+        }
+        // the right side of the array 
+        else if(arr[mid] < target){
+            low = mid + 1;
+        }
+        // the left side of the array 
+        else{
+            high = mid - 1;
+        }
+    }
+    // the answer used to return 
+    return answer;
+}
+
+
+int main(){
+    vector <int> arr = {1,2,4,4,4,6,8};
+    int target = 4;
+    int result = LastOccurrence(arr, target);
+    cout<<result;
+    return 0;
+}
+
+// the time complexity of the code is O(log n)
+// the space complexity of the code is O(1)
+
+
