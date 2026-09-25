eb6862da24c33276bda375335a46085b8618c0ee
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-25 19:30:09 +0530

    Update Count Occurrences of the element.cpp

--- Binary Search/Logic building/Count Occurrences of the element.cpp
+++ Binary Search/Logic building/Count Occurrences of the element.cpp
@@ -72,3 +72,79 @@ int main(){
 ///++++++++++++++++++++++++++++++++++++++++++++++++using the pure binray search without the lower and upper bound++++++++++++++++++++++++++++
 
 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int first_occ(vector <int> &arr, int target){
+    int n = arr.size();
+    int low = 0; 
+    int high = n - 1;
+    int first = -1;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(arr[mid] == target){
+            first = mid;
+            high = mid - 1;
+        }else if(arr[mid] > target){
+            high = mid -1 ; 
+        }else{
+            low = mid + 1;
+        }
+    }
+
+    return first;
+}
+
+int last_occ(vector <int> &arr, int target){
+    int n = arr.size();
+    int low = 0; 
+    int high = n - 1;
+    int last = -1;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(arr[mid] == target){
+            last = mid;
+            low = mid + 1;
+        }else if(arr[mid] > target){
+            high = mid -1 ; 
+        }else{
+            low = mid + 1;
+        }
+    }
+
+    return last;
+}
+
+
+
+int CountOcc(vector <int> &arr, int target){
+    int firstocc = first_occ(arr,target);
+
+    if(firstocc == -1){
+        return 0;
+    }
+
+    int lastocc = last_occ(arr,target);
+
+    return ((lastocc - firstocc )+ 1);
+
+}
+ 
+
+
+int main(){
+    vector <int> arr ={1, 1, 2, 2, 2, 2, 3};
+    int target = 2;
+    int result = CountOcc(arr,target);
+    cout << result;
+    return 0;
+}
+
+
+
+// The Time complexity of the code is O(log n)
+// The space complexity of the code is O(1)
+
+
