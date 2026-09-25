3f907045df2c69ac1a1a3a4918fa650412d408c6
AJITHKUMAR S <cloudwithajith@gmail.com>
2026-09-25 19:10:14 +0530

    Update find the first and last occurrence of the element.cpp

--- Binary Search/Logic building/find the first and last occurrence of the element.cpp
+++ Binary Search/Logic building/find the first and last occurrence of the element.cpp
@@ -1,6 +1,3 @@
-// optimal solution using the lower bound and the upper bound
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -42,16 +39,16 @@ int upperbound(vector <int> &arr, int target){
     return upper;
 }
  
-int CountOcc(vector <int> &arr, int target){
+pair<int, int> CountOcc(vector <int> &arr, int target){
     int first_occ = lowerbound(arr,target);
 
     if(arr[first_occ] != target || first_occ == (int) arr.size()){
-        return 0;
+        return pair<int, int> {0,0};
     }
 
     int last_occ = upperbound(arr,target);
 
-    return last_occ - first_occ;
+    return pair<int, int> {first_occ,last_occ - 1};
 
 }
  
@@ -60,10 +57,10 @@ int CountOcc(vector <int> &arr, int target){
 int main(){
     vector <int> arr ={1, 1, 2, 2, 2, 2, 3};
     int target = 2;
-    int result = CountOcc(arr,target);
-    cout << result;
+    pair<int,int> result = CountOcc(arr,target);
+    cout << result.first <<" "<<result.second;
     return 0;
 }
 
 // the time complexity of the code is O(log n)
-// the space complexity of the code is O(1)
+// the space cpmplexity of the code is O(1)
